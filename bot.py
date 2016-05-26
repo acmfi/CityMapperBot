d228ec55f7a49c7ce7ec638aad5fbff57ff4aca2
ironjanowar <ironjanowar@gmail.com>
2016-05-27 01:27:02 +0200

    Formated bot.py (los line too long me los paso por :heart:)

--- bot.py
+++ bot.py
@@ -3,24 +3,26 @@ import json
 from telebot import types
 
 
-#### Secure token read ####
-with open ("./acm.token", "r") as TOKEN:
+# Secure token read ####
+with open("./acm.token", "r") as TOKEN:
     bot = telebot.TeleBot(TOKEN.read())
 
 
-#### Load all text to say from json file ####
-with open ("./dataBot/text.json", "r") as data_text:
+# Load all text to say from json file ####
+with open("./dataBot/text.json", "r") as data_text:
     text = json.load(data_text)
     location_text = text['location']
 
 ########################
-##### Bot handlers #####
+# Bot handlers #####
 ########################
+
 
 @bot.message_handler(commands=['hello'])
 def help(message):
     name = message.from_user.username
     bot.reply_to(message, "Hello " + name + "!")
+
 
 @bot.message_handler(commands=['whereiam'])
 def whereiam(m):
@@ -29,20 +31,23 @@ def whereiam(m):
     teclado.row(itemLoc)
     bot.send_message(m.chat.id, location_text, reply_markup=teclado)
 
+
 @bot.message_handler(commands=['whoami'])
 def whoami(m):
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton(text=str(m.from_user.first_name), callback_data='test'))
     bot.send_message(m.chat.id, "Lo sabia", disable_notification=True, reply_markup=markup)
 
+
 @bot.message_handler(commands=['test'])
 def test(m):
     print("callback")
 
+
 @bot.message_handler(commands=['location'])
 def send_location(m):
     lat = 40.4101932
-    lon =-3.7391411,19
+    lon = - 3.7391411, 19
     bot.send_location(m.chat.id, lat, lon)
 
 bot.skip_pending = True
