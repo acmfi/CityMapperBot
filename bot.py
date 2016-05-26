a6f521b1d5cbc39dc4ecb975b4d7523245d4fc56
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2016-05-26 23:25:50 +0200

    Comments. InlineButton. Callback try. Text file.

--- bot.py
+++ bot.py
@@ -1,8 +1,21 @@
 import telebot
+import json
 from telebot import types
 
+
+#### Secure token read ####
 with open ("./acm.token", "r") as TOKEN:
     bot = telebot.TeleBot(TOKEN.read())
+
+
+#### Load all text to say from json file ####
+with open ("./dataBot/text.json", "r") as data_text:
+    text = json.load(data_text)
+    location_text = text['location']
+
+########################
+##### Bot handlers #####
+########################
 
 @bot.message_handler(commands=['hello'])
 def help(message):
@@ -14,7 +27,17 @@ def whereiam(m):
     teclado = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     itemLoc = types.KeyboardButton("Enviar mi localización", request_location=True)
     teclado.row(itemLoc)
-    bot.send_message(m.chat.id, "Para saber dónde estás, necesito que compartas conmigo tu ubicación. Si estás de acuerdo, pulsa sobre el teclado.", reply_markup=teclado)
+    bot.send_message(m.chat.id, location_text, reply_markup=teclado)
+
+@bot.message_handler(commands=['whoami'])
+def whoami(m):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton(text=str(m.from_user.first_name), callback_data='test'))
+    bot.send_message(m.chat.id, "Lo sabia", disable_notification=True, reply_markup=markup)
+
+@bot.message_handler(commands=['test'])
+def test(m):
+    print("callback")
 
 @bot.message_handler(commands=['location'])
 def send_location(m):
